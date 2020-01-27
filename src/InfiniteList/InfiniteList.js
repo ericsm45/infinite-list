63b6639823e58a3bad97a6bf09fecba620cf0e0e
Eric <ericsm45@gmail.com>
2020-01-27 16:52:11 -0700

    added list of items; beginning to track scrolling

--- src/InfiniteList/InfiniteList.js
+++ src/InfiniteList/InfiniteList.js
@@ -6,21 +6,32 @@ class InfiniteList extends React.Component {
   constructor() {
     super();
     this.state = {
-      //
+      boxList: [
+        { color:'red', animation: 1, number: 1},
+        { color:'red', animation: 1, number: 2},
+        { color:'red', animation: 1, number: 3},
+        { color:'red', animation: 1, number: 4},
+        { color:'red', animation: 1, number: 5},
+        { color:'red', animation: 1, number: 6},
+      ]
     };
     //this.checkPosition = this.checkPosition.bind(this)
     //this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
   }
   componentDidMount() {
-        
+    const listBody = document.getElementById('listBody')
+    listBody.scrollTop = 800
+    listBody.addEventListener('scroll', this.onScroll, false);
   }
 
   render( ) {
-    
+    const {boxList} = this.state;
     
     return (
-      <div>
-        <Item />
+      <div id='listBody'>
+        {boxList.map( box => {
+          return <Item key={box.number} number={box.number}/>
+        })}
       </div>
     )}
 

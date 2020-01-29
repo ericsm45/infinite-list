e6e5fd75cb932d2c1900e7071ee482716352f176
Eric <ericsm45@gmail.com>
2020-01-28 20:29:56 -0700

    adds 8 to the end of list; mobile friendly

--- src/InfiniteList/InfiniteList.js
+++ src/InfiniteList/InfiniteList.js
@@ -1,36 +1,67 @@
 import React from "react";
-
 import Item from '../Item/Item'
 
 class InfiniteList extends React.Component {
   constructor() {
     super();
     this.state = {
-      boxList: [
-        { color:'red', animation: 1, number: 1},
-        { color:'red', animation: 1, number: 2},
-        { color:'red', animation: 1, number: 3},
-        { color:'red', animation: 1, number: 4},
-        { color:'red', animation: 1, number: 5},
-        { color:'red', animation: 1, number: 6},
-      ]
+      itemList: [
+        { number: 1},
+        { number: 2},
+        { number: 3},
+        { number: 4},
+        { number: 5},
+        { number: 6},
+        { number: 7},
+        { number: 8},
+      ],
+      itemIndex: 8
     };
-    //this.checkPosition = this.checkPosition.bind(this)
-    //this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.onScroll = this.onScroll.bind(this)
+    this.addItems = this.addItems.bind(this);
   }
   componentDidMount() {
     const listBody = document.getElementById('listBody')
-    listBody.scrollTop = 800
     listBody.addEventListener('scroll', this.onScroll, false);
+  }
+  addItems(){
+    var {itemList, itemIndex} = this.state
+    var newItemList = itemList
+    newItemList.push(
+      { number: itemIndex + 1},
+      { number: itemIndex + 2},
+      { number: itemIndex + 3},
+      { number: itemIndex + 4},
+      { number: itemIndex + 5},
+      { number: itemIndex + 6},
+      { number: itemIndex + 7},
+      { number: itemIndex + 8}
+    )
+    
+    this.setState({
+      itemList: newItemList,
+      itemIndex: itemIndex + 8
+    })
+  }
+  onScroll(){
+    const itemList = document.getElementById('listBody')
+    if(itemList.scrollTop > (itemList.scrollHeight - 900)){
+      this.addItems()
+    }
   }
 
   render( ) {
-    const {boxList} = this.state;
-    
+    const {itemList} = this.state;
+
     return (
       <div id='listBody'>
-        {boxList.map( box => {
-          return <Item key={box.number} number={box.number}/>
+        <div className='listDescription'>
+          
+          <div>Scroll down to increase list</div>
+          <div>(Length: {itemList.length})</div>
+        </div>
+        {itemList.map( item => {
+          return <Item key={item.number} number={item.number}/>
         })}
       </div>
     )}
--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,9 +1,8 @@
 import React from "react";
-//import { Link } from 'react-router-dom'
 
-const Item = () => (
+const Item = ({number}) => (
     <div className='item'>
-        Test Item
+        {number}
     </div>
 );
 
